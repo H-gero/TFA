eaa12caa33317a3bf7344b9e7874a0cf23f814d5
erickdevup <erickdevup@gmail.com>
2024-07-01 18:12:06 +0200

    change/addEntry done

--- app/dashboard/entries/EntriesProvider.tsx
+++ app/dashboard/entries/EntriesProvider.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { Entry, EntryState, EntryStatus } from "../interface/entry";
+import { Entry, EntryState } from "../interface/entry";
 import { EntriesContext } from "./EntriesContext";
 import { useEffect, useReducer } from "react";
 import { EntriesReducer } from "./EntriesReducer";
 import { getEntry } from "../actions/services/getEntry";
 import { parseEntry } from "../utils/parseEntry";
+import { changeEntry } from "../actions/services/changeEntry";
 
 
 export interface EntriesState {
@@ -22,37 +23,28 @@ export default function EntriesProvider({ children }: Readonly<{
     const [state, dispatch] = useReducer(EntriesReducer, { entries: [] }); // Estado inicial vacío o mínimo necesario
 
     useEffect(() => {
-      getEntry('api/entries/')
-       .then((response) => {
-          const data = response;
-          
-          if (Array.isArray(data)) {
-            const parsedData = parseEntry(data); // Asume que parseEntry devuelve un objeto con la estructura correcta para tu estado
-            dispatch({ type: '[Entry] -initial-entries', payload: parsedData.entries });
-          } else {
-            console.error('La respuesta no es un array:', data);
-          }
-        })
-       .catch(error => {
-          console.error('Error al cargar las entradas:', error);
-        });
+        getEntry('api/entries/')
+            .then((response) => {
+                const data = response;
+                const parsedData = parseEntry(data); // Asume que parseEntry devuelve un objeto con la estructura correcta para tu estado
+                dispatch({ type: '[Entry] -initial-entries', payload: parsedData.entries });
+            })
+            .catch(error => {
+                console.error('Error al cargar las entradas:', error);
+            });
     }, []);
 
-    const addNewEntry = (description: string, state: EntryState, cantidad: number) => {
-        const newEntry: Entry = {
-            _id: '7',
-            cant: cantidad,
-            description,
-            createAt: Date.now(),
-            status: 'Bien',
-            state: state
-        }
+     const addNewEntry = async (description: string, state: EntryState, cantidad: number) => {
+       
+        const data = await changeEntry('api/entries/', { cant: cantidad, description, state: state })
+        const parsedData = parseEntry([data])
+        dispatch({ type: '[Entry] -Add-Entry', payload: parsedData.entries[0] })
 
-        dispatch({ type: '[Entry] -Add-Entry', payload: newEntry })
     }
 
     const updateEntry = (entry: Entry, cant: number) => {
         dispatch({ type: '[Entry] -Entry-Update', payload: entry, cantidad: cant })
+        
     }
 
     const deleteEntry = (entry: Entry, index: number) => {
