b8d89a81e6dda6a9392fd3bf84c3c5ea0cd17bc5
erickdevup <erickdevup@gmail.com>
2024-07-01 17:25:32 +0200

    getEntries done

--- app/dashboard/entries/EntriesProvider.tsx
+++ app/dashboard/entries/EntriesProvider.tsx
@@ -2,72 +2,41 @@
 
 import { Entry, EntryState, EntryStatus } from "../interface/entry";
 import { EntriesContext } from "./EntriesContext";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { EntriesReducer } from "./EntriesReducer";
+import { getEntry } from "../actions/services/getEntry";
+import { parseEntry } from "../utils/parseEntry";
 
 
 export interface EntriesState {
     entries: Entry[];
 }
 
-const ENTRIES_INITIAL_STATE: EntriesState = {
-    entries: [
-        {
-            _id: '1',
-            cant: 10,
-            description: 'Laptop',
-            status: 'Bien',
-            createAt: Date.now(),
-            state: 'Tecnológico'
-        },
-        {
-            _id: '2',
-            cant: 20,
-            description: 'Sofa',
-            status: 'Regular',
-            createAt: Date.now() - 1000000,
-            state: 'Mobiliario'
-        },
-        {
-            _id: '3',
-            cant: 40,
-            description: 'Libreta',
-            status: 'Mal',
-            createAt: Date.now() - 100000,
-            state: 'Útil'
-        },
-        {
-            _id: '4',
-            cant: 30,
-            description: 'Torre',
-            status: 'Regular',
-            createAt: Date.now(),
-            state: 'Tecnológico'
-        },
-        {
-            _id: '5',
-            cant: 60,
-            description: 'Cama',
-            status: 'Mal',
-            createAt: Date.now() - 1000000,
-            state: 'Mobiliario'
-        },
-        {
-            _id: '6',
-            cant: 100,
-            description: 'Borrador',
-            status: 'Bien',
-            createAt: Date.now() - 100000,
-            state: 'Útil'
-        }
-    ]
-}
+
+
+
 
 export default function EntriesProvider({ children }: Readonly<{
     children: React.ReactNode;
-  }>){
+}>) {
+    const [state, dispatch] = useReducer(EntriesReducer, { entries: [] }); // Estado inicial vacío o mínimo necesario
 
-    const [state, dispatch] = useReducer(EntriesReducer, ENTRIES_INITIAL_STATE);
+    useEffect(() => {
+      getEntry('api/entries/')
+       .then((response) => {
+          const data = response;
+          
+          if (Array.isArray(data)) {
+            const parsedData = parseEntry(data); // Asume que parseEntry devuelve un objeto con la estructura correcta para tu estado
+            dispatch({ type: '[Entry] -initial-entries', payload: parsedData.entries });
+          } else {
+            console.error('La respuesta no es un array:', data);
+          }
+        })
+       .catch(error => {
+          console.error('Error al cargar las entradas:', error);
+        });
+    }, []);
 
     const addNewEntry = (description: string, state: EntryState, cantidad: number) => {
         const newEntry: Entry = {
@@ -79,30 +48,31 @@ export default function EntriesProvider({ children }: Readonly<{
             state: state
         }
 
-        dispatch({type: '[Entry] -Add-Entry', payload: newEntry})
+        dispatch({ type: '[Entry] -Add-Entry', payload: newEntry })
     }
 
     const updateEntry = (entry: Entry, cant: number) => {
-        dispatch({type: '[Entry] -Entry-Update', payload:entry , cantidad: cant })
+        dispatch({ type: '[Entry] -Entry-Update', payload: entry, cantidad: cant })
     }
 
     const deleteEntry = (entry: Entry, index: number) => {
-        dispatch({type: '[Entry] -Entry-Delete', payload: entry , indice: index})
+        dispatch({ type: '[Entry] -Entry-Delete', payload: entry, indice: index })
     }
     const SearchUpdate = (cad: string, entries: Entry[]) => {
-        dispatch({type: '[Entry] -Search-Entry-Update', cadena: cad, payload: entries})
+        dispatch({ type: '[Entry] -Search-Entry-Update', cadena: cad, payload: entries })
     }
-  return (
-    <EntriesContext.Provider value={
-        {...state,
-            addNewEntry,
-            updateEntry,
-            deleteEntry,
-            SearchUpdate,
-        }
+    return (
+        <EntriesContext.Provider value={
+            {
+                ...state,
+                addNewEntry,
+                updateEntry,
+                deleteEntry,
+                SearchUpdate,
+            }
         }>
-        {children}
-    </EntriesContext.Provider>
-  )
+            {children}
+        </EntriesContext.Provider>
+    )
 }
 
